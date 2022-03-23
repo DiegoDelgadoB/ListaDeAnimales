2bc8364c273f342024a80d2c61b9127fe3d1c4d7
Diego Delgado Barra <diego.delgadobarra@outlook.com>
2022-03-23 13:03:27 -0300

    commit ProgramaFuncionando

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,7 @@ class Propietario {
         this.telefono = telefono;
     }
 
+    datosPropietario() {
+        return `El nombre del dueño es: <strong>${this.nombre}</strong>. El domicilio es: <strong>${this.direccion}</strong>, y el número de telefóno de contacto es: <strong>${this.telefono}</strong>`
+    }
+}
